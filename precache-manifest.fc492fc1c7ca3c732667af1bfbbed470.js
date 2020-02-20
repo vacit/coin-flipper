bd2a54d876bd1baac5477de13620b5b6063d8412
vacit <vacitabdulrazak@gmail.com>
2020-02-20 12:57:09 +0300

    Updates

---
+++ precache-manifest.fc492fc1c7ca3c732667af1bfbbed470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0888128f30a3cafe9830e426fd4880",
+    "url": "/coin-flipper/index.html"
+  },
+  {
+    "revision": "c60a2746225312ff4795",
+    "url": "/coin-flipper/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "4f9f91fff8131483efcf",
+    "url": "/coin-flipper/static/js/2.5af7eab3.chunk.js"
+  },
+  {
+    "revision": "c60a2746225312ff4795",
+    "url": "/coin-flipper/static/js/main.612c8ec7.chunk.js"
+  },
+  {
+    "revision": "26f54948e695e8d62fd7",
+    "url": "/coin-flipper/static/js/runtime~main.556832e0.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/coin-flipper/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/coin-flipper/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
